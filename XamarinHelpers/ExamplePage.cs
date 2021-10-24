f39b27e999e4be65389cb3c08b8aa7d9f8de5819
danny <danny.dowling@gmail.com>
2021-10-24 06:40:10 -0800

    Adding in helper methods for working with Xamarin.

---
+++ XamarinHelpers/ExamplePage.cs
@@ -0,0 +1,76 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinHelpers
+{
+    public class ExamplePage : ContentView
+    {
+        public ExamplePage()
+        {
+            var Exit = new PictureAsButton();
+            Exit.Icon = "MenuExit.png";
+            Exit.Label = "Exit";
+            var ExittapGestureRecognizer = new TapGestureRecognizer();
+            Exit.GestureRecognizers.Add(ExittapGestureRecognizer);
+            ExittapGestureRecognizer.Tapped += (s, e) =>
+            {
+                Environment.Exit(0);
+            };
+
+            ZoomContainer zoom = new ZoomContainer();
+            Image aboutinfo = new Image();
+            aboutinfo.Source = "aboutinfo.png";
+            zoom.HeightRequest = 2000;
+            zoom.Content = aboutinfo;
+
+            Grid grid = new Grid();
+
+            RowDefinition rowDef1 = new RowDefinition();
+            RowDefinition rowDef2 = new RowDefinition();
+            RowDefinition rowDef3 = new RowDefinition();
+
+            grid.RowDefinitions.Add(rowDef1);
+            grid.RowDefinitions.Add(rowDef2);
+            grid.RowDefinitions.Add(rowDef3);
+
+
+            ColumnDefinition colDef1 = new ColumnDefinition();
+            ColumnDefinition colDef2 = new ColumnDefinition();
+
+            grid.ColumnDefinitions.Add(colDef1);
+            grid.ColumnDefinitions.Add(colDef2);
+
+            Grid.SetRow(Exit, 0);
+            Grid.SetColumn(Exit, 0);
+            Grid.SetRow(aboutinfo, 0);
+            Grid.SetColumn(aboutinfo, 1);
+
+            grid.Children.Add(Exit);
+            grid.Children.Add(aboutinfo);
+
+            var segments = new StackLayout
+            {
+                Spacing = 0,
+                Orientation = StackOrientation.Vertical,
+                BackgroundColor = Color.White,
+                Children = { grid, zoom}
+            };
+
+            //var view = new RelativeLayout();
+            //{
+            //    view.Children.Add(segments,
+            //    xConstraint: Constraint.Constant(0),
+            //    yConstraint: Constraint.Constant(0),
+            //    widthConstraint: Constraint.RelativeToParent((parent) => { return parent.Width; }),
+            //    heightConstraint: Constraint.RelativeToParent((parent) => { return parent.Height; }));
+            //};
+
+            Content = new ScrollView
+            {
+                BackgroundColor = Color.White,
+                Orientation = ScrollOrientation.Both,
+                Content = segments
+            };
+        }
+    }
+}
+           
---
+++ XamarinHelpers/Helpers/PictureAsButton.cs
@@ -0,0 +1,41 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinHelpers
+{
+    public class PictureAsButton : ContentView
+    {
+        public Image imageIcon { get; set; }
+        public Label imageLabel { get; set; }
+        public ImageSource Icon
+
+        {
+            get { return imageIcon.Source; }
+            set { imageIcon.Source = value; }
+        }
+        public string Label
+        {
+            get { return imageLabel.Text; }
+            set { imageLabel.Text = value; }
+        }
+        public PictureAsButton()
+        {
+            imageIcon = new Image();
+            imageIcon.Source = "{Binding Icon}";
+            imageIcon.HorizontalOptions = LayoutOptions.Center;
+
+            imageLabel = new Label();
+            imageLabel.Text = "{Binding Label}";
+            imageLabel.TextColor = Color.Black;
+            imageLabel.HorizontalOptions = LayoutOptions.Center;
+
+            Content = new StackLayout
+            {
+                Spacing = 10,
+                Orientation = StackOrientation.Vertical,
+                BackgroundColor = Color.White,
+                Children = { imageIcon, imageLabel }
+            };
+
+        }
+    }
+}
