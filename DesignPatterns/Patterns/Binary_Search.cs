ce9a8599ef63cdcc254380a0387ad2903f7ab0cc
Danny Dowling <danny.dowling@gmail.com>
2021-11-20 19:47:29 -0900

    fix

--- DesignPatterns/Patterns/Binary_Search.cs
+++ DesignPatterns/Patterns/Binary_Search.cs
@@ -11,52 +11,61 @@ namespace DesignPatterns.Patterns
     internal class Binary_Search
     {
         private T BinarySearch<T>(IEnumerable<T> list, T key) where T : IComparable
-        {           
-            //we're going to need two sets of data to reduce quickly by
+        {
+            //First we'll sort the enumeration into a new structure so we can find anything in it
+            SortedList<int, T> allT = new SortedList<int, T>();
+
+            for (int i = 0; i < list.Count() - 1; i++)
+            {
+                allT.Add(i, list.ElementAt(i));
+            }
+
+            //we want to split this up to make searching faster
             SortedList<int, T> left = new SortedList<int, T>();
             SortedList<int, T> right = new SortedList<int, T>();
 
-            for (int i = 0; i < list.Count(); i++)
+            for (int i = 0; i < allT.Count(); i++)
             {
                 if (i < list.Count() /2)
                 {
-                    left.Add(i, list.ElementAt(i));
+                    left.Add(i, allT.ElementAt(i).Value);
                 }
                 else
                 {
-                    right.Add(i, list.ElementAt(i));
+                    right.Add(i, allT.ElementAt(i).Value);
                 }
             }
             
 
             // start in the middle of the list
-            for (int i = list.Count() / 2; i < list.Count() - 1; i++)
+            for (int i = allT.Count() / 2; i < allT.Count() - 1; i++)
             {
-                T item = list.ElementAt(i);  
+                T item = allT.ElementAt(i).Value;  
 
                 var comparison = key.CompareTo(item); 
                 if (comparison == 0)
                 {
-                    return list.ElementAt(i);  //if it's the one selected, then return
+                    return allT.ElementAt(i).Value;  //if it's the one selected, then return
                 }
 
                 if (comparison < 0) //if the thing is in the left list
                 {
-                   //we're going to drop the right list.
-                    for (int l = left.Count()/2; l < left.Count() - 1; l--)
+                    right = new SortedList<int, T>(); //drop all the entries in the right
+                    
+                    for (int l = left.Count() / 2; l < left.Count() - 1; l--)
                     {
-                        right.Add(l, left[l]); //adding in all the last indexers from the left to the right
-                        left.Remove(l);
+                        right.Add(l, left[l]);                   //fill up the right with the last part of the left list
+                        left.RemoveAt(l);                        //and remove what is now in the right list from the left
                     }
 
                 }
-                else //or the item's in the right list
+                else 
                 {
-                    //we're going to drop the right list.
+                    left = new SortedList<int, T>();                   
                     for (int r = left.Count() / 2; r < right.Count() - 1; r++)
                     {
-                        left.Add(r, right[r]); //adding in all the last indexers from the right to the left
-                        right.Remove(r);
+                        left.Add(r, right[r]); 
+                        right.RemoveAt(r);
                     }
                 }
 
