0c0d54793e5cf1f1c79162d2ded118af99cf149c
Danny Dowling <danny.dowling@gmail.com>
2021-11-20 19:53:22 -0900

    fix

--- DesignPatterns/Patterns/Binary_Search.cs
+++ DesignPatterns/Patterns/Binary_Search.cs
@@ -16,9 +16,7 @@ namespace DesignPatterns.Patterns
             SortedList<int, T> sortedListT = new SortedList<int, T>();
 
             for (int i = 0; i < list.Count() - 1; i++)
-            {
-                sortedListT.Add(i, list.ElementAt(i));
-            }
+            { sortedListT.Add(i, list.ElementAt(i));   }
 
             //we want to split this up to make searching faster
             SortedList<int, T> left = new SortedList<int, T>();
@@ -27,52 +25,37 @@ namespace DesignPatterns.Patterns
             for (int i = 0; i < sortedListT.Count(); i++)
             {
                 if (i < list.Count() /2)
-                {
-                    left.Add(i, sortedListT.ElementAt(i).Value);
-                }
+                {  left.Add(i, sortedListT.ElementAt(i).Value);  }
                 else
-                {
-                    right.Add(i, sortedListT.ElementAt(i).Value);
-                }
-            }
+                {  right.Add(i, sortedListT.ElementAt(i).Value); }
+            }            
             
-
-            // start in the middle of the list
-            for (int i = sortedListT.Count() / 2; i < sortedListT.Count() - 1; i++)
+            for (int i = sortedListT.Count() / 2; i < sortedListT.Count() - 1; i++) // start in the middle of the list
             {
                 T item = sortedListT.ElementAt(i).Value;  
 
                 var comparison = key.CompareTo(item); 
                 if (comparison == 0)
-                {
-                    return sortedListT.ElementAt(i).Value;  //if it's the one selected, then return
-                }
+                {  return sortedListT.ElementAt(i).Value; } //when it's not different, it's the one
+
 
                 if (comparison < 0) //if the thing is in the left list
-                {
-                    right = new SortedList<int, T>(); //drop all the entries in the right
+                {  right = new SortedList<int, T>(); //drop all the entries in the right
                     
                     for (int l = left.Count() / 2; l < left.Count() - 1; l--)
-                    {
-                        right.Add(l, left[l]);                   //fill up the right with the last part of the left list
-                        left.RemoveAt(l);                        //and remove what is now in the right list from the left
-                    }
-
+                    {  right.Add(l, left[l]);                   //fill up the right with the last part of the left list
+                        left.RemoveAt(l);   }                   //and remove what is now in the right list from the left
                 }
                 else 
-                {
-                    left = new SortedList<int, T>();                   
+                {   left = new SortedList<int, T>();                   
                     for (int r = left.Count() / 2; r < right.Count() - 1; r++)
                     {
                         left.Add(r, right[r]); 
                         right.RemoveAt(r);
-                    }
-                }
-
-                return list.ElementAt(i);
-            }
-            return default(T);
-            
+                    }  }                
+                return sortedListT.ElementAt(i).Value; //return the found key
+            }            
+            return default(T);            //or return the default
         }
     }
 }
