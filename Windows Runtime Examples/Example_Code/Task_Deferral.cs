ffbae84026b121b11b03a76633f1f51593218ffe
Danny Dowling <danny.dowling@gmail.com>
2022-03-09 18:35:53 -0900

    working on fixing the errors with types mismatching

--- Windows Runtime Examples/Example_Code/Task_Deferral.cs
+++ Windows Runtime Examples/Example_Code/Task_Deferral.cs
@@ -1,42 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.ApplicationModel;
+using Windows.Storage;
 
 namespace Windows_Runtime_Examples.Example_Code
 {
-    internal class Task_Deferral
+    internal class Defer_A_Task
     {
         SuspendingDeferral? deferral;
         SuspendingOperation? _operation;
-
-        // The SuspendingOperation is a implementation of SuspendingDeferral.
-        // There's a lot of code implemented behind the scenes to make this work.
-    
-            private async void Defer_Execution(SuspendingOperation operation, Task t)
+            
+            private async void Defer_Execution_Return(Task t)
         {
-            // the suspending operation is what will trigger the deferral
-            _operation = operation;
-
-
-            // set the task to do the work
-           await t.ContinueWith(t => setTheThreadToWork());
-
             if (!t.IsCompleted)
-            { 
-                _operation.GetDeferral();             
-            }
-             else
-            {
-                deferral.Complete();                
-            }            
+            { _operation?.GetDeferral();
+                // set the task to do the work
+                await t.ContinueWith(t => setTheThreadToWork());
+            }             
+         deferral?.Complete(); 
         }
 
         private async void setTheThreadToWork()
         {
-            await Task.Delay(2000);
+           await Task.Delay(2000);
+        }
+    }
+
+    internal class Save_State_With_Deferral
+    {
+        public Save_State_With_Deferral(RuntimeMethodHandle s, RuntimeArgumentHandle e)
+        {
+            // error is that it can't convert from RuntimeArgumentHandle to SuspendedEventArgs
+            OnSuspendingEvent(s, e);            
+        }
+
+        private void Lock_ArgumentArray(RuntimeArgumentHandle e)
+        {
+            Array resultArray = Array.Empty<object>();
+            resultArray.CopyTo(e, 0);
+            lock (resultArray) ;
+        }
+
+        private Dictionary<string, object> _suspended_State = new Dictionary<string, object>();
+        
+        private readonly string _saveFileName = "store.xml";
+
+        private async void OnSuspendingEvent(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();         
+            await SaveStateAsync();
+            deferral.Complete();
+        }
+        private async Task SaveStateAsync()
+        {
+            var memoryStream = new MemoryStream();
+            var serializer = new DataContractSerializer(typeof(Dictionary<string, object>));
+            serializer.WriteObject(memoryStream, _suspended_State);
+
+            var file = await ApplicationData.Current.LocalFolder.CreateFileAsync(_saveFileName, CreationCollisionOption.ReplaceExisting);
+
+            using (var fileStream = await file.OpenStreamForWriteAsync())
+            {
+                //because we have written to the stream, set the position back to start
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                await memoryStream.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
+            }
         }
     }
 }
