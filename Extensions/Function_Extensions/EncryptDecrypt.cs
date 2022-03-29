14b3200bd6c403a8f2b0c504761a94d43c17a4c2
Danny Dowling <danny.dowling@gmail.com>
2022-03-28 18:39:59 -0800

    working with cryptography

--- Extensions/Function_Extensions/EncryptDecrypt.cs
+++ Extensions/Function_Extensions/EncryptDecrypt.cs
@@ -1,33 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
+﻿using Extensions.Type_Extensions;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DesignPatterns.Extensions.Function_Extensions
 {
-    public static class EncryptDecrypt
+    public static class Encrypt
     {
-        public static string Encrypt(this string stringToEncrypt, string key)
+        public static string ByRSA<T>(this T objectToEncrypt, string key) where T: IFormattable
         {
-            var cspParameter = new CspParameters { KeyContainerName = key };
-            var rsaServiceProvider = new RSACryptoServiceProvider(cspParameter) { PersistKeyInCsp = true };
-            byte[] bytes = rsaServiceProvider.Encrypt(Encoding.UTF8.GetBytes(stringToEncrypt), true);
+            CspParameters cspParameter = new CspParameters { KeyContainerName = key };
+            RSACryptoServiceProvider rsaServiceProvider = new RSACryptoServiceProvider(cspParameter) { PersistKeyInCsp = true };
+            byte[] bytes = rsaServiceProvider.Encrypt(objectToEncrypt.ObjectToByteArray(), true);
             return BitConverter.ToString(bytes);
         }
 
-        public static string Decrypt(this string stringToDecrypt, string key)
+        public static string ByAES<T>(this T objectToEncrypt, string key) where T: IFormattable
         {
-            var cspParamters = new CspParameters { KeyContainerName = key };
-            var rsaServiceProvider = new RSACryptoServiceProvider(cspParamters) { PersistKeyInCsp = true };
-            string[] decryptArray = stringToDecrypt.Split(new[] { "-" }, StringSplitOptions.None);
-            byte[] decryptByteArray = Array.ConvertAll(decryptArray,
-                (s => Convert.ToByte(byte.Parse(s, NumberStyles.HexNumber))));
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(objectToEncrypt);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        } 
+    }
+
+    public static class Decrypt { 
+
+        public static string ByRSA<T>(string key , T objectToDecrypt) where T: IFormattable
+        {
+            CspParameters cspParamters = new CspParameters { KeyContainerName = key };
+            RSACryptoServiceProvider rsaServiceProvider = new RSACryptoServiceProvider(cspParamters) { PersistKeyInCsp = true };
+            //string[] decryptArray = objectToDecrypt.ToString().Split(new[] { "-" }, StringSplitOptions.None);
+            //byte[] decryptByteArray = Array.ConvertAll(decryptArray,
+            //    (s => Convert.ToByte(byte.Parse(s, NumberStyles.HexNumber))));
+
+            byte[] decryptByteArray = objectToDecrypt.ObjectToByteArray();
             byte[] bytes = rsaServiceProvider.Decrypt(decryptByteArray, true);
             string result = Encoding.UTF8.GetString(bytes);
             return result;
         }
+
+        public static string ByAES<T>(string key, T objectToDecrypt) where T: IFormattable
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = objectToDecrypt.ObjectToByteArray();
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader(cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+       
     }
 }
+
---
+++ Extensions/Type_Extensions/ToFromByteArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions.Type_Extensions
+{
+    public static class ToFromByteArray
+    {
+        // Convert an object to a byte array
+        public static byte[] ObjectToByteArray<T>(this T obj) where T: IFormattable
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using (var ms = new MemoryStream())
+            {
+                bf.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+
+        // Convert a byte array to an Object
+        public static Object ByteArrayToObject<T>(this byte[] arrBytes)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                var binForm = new BinaryFormatter();
+                memStream.Write(arrBytes, 0, arrBytes.Length);
+                memStream.Seek(0, SeekOrigin.Begin);
+                var obj = binForm.Deserialize(memStream);
+                return obj;
+            }
+        }
+    }
+}
