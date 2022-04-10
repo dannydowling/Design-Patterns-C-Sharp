047a663833a946088612003ddeb27b64a9b3aad8
Danny Dowling <danny.dowling@gmail.com>
2022-04-09 17:20:56 -0800

    added parallel file copy and stream copy.

---
+++ Code Snippets/Code Snippets/AsyncFileCopy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Snippets.Code_Snippets
+{
+    public static class AsyncFileCopy
+    {
+        public static async Task CopyFileAsync(List<string> sources, string destinationFile, CancellationToken cancellationToken)
+        {
+            var fileOptions = FileOptions.Asynchronous | FileOptions.SequentialScan;
+            var bufferSize = 4096;
+
+            Parallel.For(0, sources.Count, async i =>
+{
+    using (var sourceStream =
+          new FileStream(sources[i], FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize, fileOptions))
+
+
+    using (var destinationStream =
+          new FileStream(destinationFile, FileMode.CreateNew, FileAccess.Write, FileShare.None, bufferSize, fileOptions))
+
+        await sourceStream.CopyToAsync(destinationStream, bufferSize, cancellationToken)
+                                   .ConfigureAwait(continueOnCapturedContext: false);
+});
+        }
+    }
+}
---
+++ Code Snippets/Code Snippets/AsyncStreamCopy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Snippets.Code_Snippets
+{
+    public static class AsyncStreamCopy
+    {
+        public async static void CopyStreamsToStream(List<Stream> sources, Stream destination, Action<Stream, Stream, Exception> completed)
+        {
+            // set up the buffer
+            byte[] buffer = new byte[0x1000];
+
+            // create a thing to flag for when the task is done
+            Action<Exception> done = e =>
+            {
+                if (completed == null)
+                {
+                    Task.FromException(e);
+                }
+            };
+
+            // the actual file copy system
+            AsyncCallback rc = readResult =>
+            {
+                try
+                {
+                    foreach (var source in sources)
+                    {
+                        int read = source.EndRead(readResult);
+                        if (read > 0)
+                        {
+                            destination.BeginWrite(buffer, 0, read, writeResult =>
+                            {
+                                try
+                                {
+                                    // write the buffer then read the next part
+                                    destination.WriteAsync(buffer, 0, read);
+                                    destination.ReadAsync(buffer, 0, buffer.Length);
+                                }
+                                catch (Exception exc) { done(exc); }
+                            }, null);
+                        }
+                    }
+                }
+                catch (Exception exc) { done(exc); }
+            };
+            // for each source, read the buffer
+            sources.ForEach(x => x.BeginRead(buffer, 0, buffer.Length, rc, null));
+        }
+    }
+}
