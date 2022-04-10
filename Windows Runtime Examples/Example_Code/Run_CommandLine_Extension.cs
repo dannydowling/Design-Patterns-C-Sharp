ba2fcd10ec24bfbfc4bf4371b14b925ddba8b78e
Danny Dowling <danny.dowling@gmail.com>
2022-04-09 20:49:03 -0800

    run from command line extension added.

---
+++ Windows Runtime Examples/Example_Code/Run_CommandLine_Extension.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_Runtime_Examples.Example_Code
+{
+    public static class Run_CommandLine_Extension
+    {
+        public static string[] unfilteredData { get; set; }
+
+        public static Action Run_CommandLine(this string command, List<object> dataSet)
+        {
+            using (Process process = new Process())
+            {
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "cmd.exe");
+                process.StartInfo.RedirectStandardInput = true;              
+
+                process.OutputDataReceived += ProcessOutputDataHandler;
+                process.ErrorDataReceived += ProcessErrorDataHandler;
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                Action p = () =>
+                {
+                    foreach (var operand in dataSet)
+                    {
+                        process.StandardInput.WriteLine("{0} {1}", command, operand);
+                    }
+                };
+                return p;
+            }
+        }
+
+        public static void ProcessOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            unfilteredData.Append(outLine.Data);
+            Console.WriteLine(outLine.Data);
+        }
+
+        public static void ProcessErrorDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            Console.WriteLine(outLine.Data);
+        }
+    }
+}
+
